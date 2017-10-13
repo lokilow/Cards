f6a4ca6bb74428eeceebd6ac8818f80b08c63161
Chris Lowen <lokilow@protonmail.com>
2017-10-13 01:13:57 -0400

    Add very basic IO

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,30 @@
 module Main where
 
 import Lib
+import Text.Printf
+
+--Enumerated list of implemented games
+--TODO implement these games
+games = zip [0..] ["go fish", "war"]
+
+showGame :: (Int, String) -> IO ()
+showGame (num, game) = printf "%d  -  %s\n" num game
+
+chooseGame :: IO Int
+chooseGame = do
+    putStrLn "Please choose a game by selecting its number"
+    mapM_ showGame games
+    pick <- getLine
+    case reads pick :: [(Int,String)] of
+      [(n, _)] -> 
+          if (elem n (map fst games)) then return n else do
+             putStrLn "Invalid Input"
+             chooseGame
+      _ -> do
+          putStrLn "invalid input"
+          chooseGame
 
 main :: IO ()
-main = someFunc
+main = do
+    game <- chooseGame
+    putStrLn $ "Playing " ++ snd (games !! game)
